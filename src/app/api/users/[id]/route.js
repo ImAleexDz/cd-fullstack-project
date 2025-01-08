1cc241dfe13e30cf62e209d878c71f059d4c8ff6
Alejandro Diaz Alvarado <alejandrodiazalvarado@Alejandros-MacBook-Air.local>
2025-01-07 20:49:00 -0600

    delete method

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -3,7 +3,7 @@ import { data } from '../../../../lib/data'
 
 const getUserById = (id) => {
     return data.users.find((user) => {
-        return user.id == id
+        return user ?user.id == id : null
     })
 }
 
@@ -19,6 +19,35 @@ export async function GET(request, context) {
     }
 
     return new Response(JSON.stringify(user), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+//Delete user 
+
+const deleteUserById = (id) => {
+    const index = data.users.findIndex(usuario => usuario.id == id);
+
+    if (index !== -1) {
+        return delete data.users[index]
+    }
+}
+
+export async function DELETE(request, context) {
+    const id = await context.params.id;
+
+    const user = deleteUserById(id);
+
+    if(!user) {
+        return new Response(null, {
+            status: 404
+        })
+    }
+
+    return new Response(JSON.stringify({meesage: 'Usuario borrado con éxito'}), {
         status: 200,
         headers: {
             'Content-Type': 'application/json'
